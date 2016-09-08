3f09e4a8ff350eb36b57109c545db7ab11c6b2ee
Elysia Nason and Karen Freeman-Smith <karen@>
2016-09-08 13:51:55 -0700

    add test for object creation, status red fail

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class GameTest {
-  
+
+  @Test
+  public void newGame_createsObject_true() {
+    Game thisGame = new Game();
+    assertEquals(true, thisGame instanceof Game);
+  }
 }
